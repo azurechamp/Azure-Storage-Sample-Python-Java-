dd7bff3df0660694140b8bc1653c54b75ada1ba7
saad.mahmood <saad.mahmood@LHRLT-1292>
2017-01-06 18:49:42 +0500

    Add project files.

---
+++ AzureStorage/AzureStorage.py
@@ -0,0 +1,56 @@
+import sys
+from azure.storage.blob.blockblobservice import BlockBlobService
+from azure.storage import *
+from azure.storage.blob import ContentSettings
+
+
+
+
+block_blob_service = BlockBlobService(account_name='azuremvp', account_key='o4TFKR/k006Akal0EgW7LUYxzLpnchr6kJw1cIQi2SqoDb6t3nSQ5w4bhp8OLTseTUGRUbhJfp36i2iJgn/M4w==');
+a="asdfad";
+
+#------------------------- Credentials ---------------------------#
+#                                                                 #
+# You can try using this filename and type to upload sample Image #
+# fileName : 'storage.png'                                        #
+# type : 'image/png'                                              #
+#-----------------------------------------------------------------#
+
+
+def CreateBlob(name,container,type,file):
+    block_blob_service.create_blob_from_path(
+    container,
+    name,
+    file,
+    content_settings= ContentSettings(content_type=type)
+            );
+
+
+def ListBlobs(container):
+    generator = block_blob_service.list_blobs(container)
+    for blob1 in generator:
+        print(blob1.name)
+
+def DownloadBlob(container,blobname,filepath):
+    block_blob_service.get_blob_to_path(container,blobname,filepath)
+
+def WriteBlobinText(container,blobname,encodingtype):
+    block_blob_service.get_blob_to_text(container, blobname, encodingtype)
+    
+    
+def DeleteBlob(container,blobname):
+    block_blob_service.delete_blob(container, blobname)
+  
+def DeleteContainer(container):
+    block_blob_service.delete_container(container)
+
+def CreateBlobFromBytes(container,blobname,bytesContent):
+    block_blob_service.create_blob_from_bytes(container,blobname,bytesContent);
+
+     
+def main():
+    print("YourTestHer")
+    input()   
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
